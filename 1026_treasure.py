8d88539943671c21004f23044c7ee51f24916096
skfkeh <>
2023-04-04 18:16:08 +0900

    first commit

---
+++ 1026_treasure.py
@@ -0,0 +1,18 @@
+n = int(input())
+a = input()
+b = input()
+
+alist = a.split()
+aalist = list(map(int, alist))
+aalist.sort(reverse = True)
+
+blist = b.split()
+bblist = list(map(int, blist))
+bblist.sort()
+
+total = 0
+
+for i in range(n):
+    total += aalist[i]*bblist[i]
+    
+print(total)
---
+++ 13627_DangerousDive.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+who = input()
+
+survived = who.split()
+survived.sort()
+dead = []
+
+for i in range(1, n+1):
+    if str(i) in survived:
+        pass
+    else:
+        dead.append(i)
+
+if len(dead) == 0:
+    print("*")
+else:
+    for i in dead:
+        print(i, end=" ")
