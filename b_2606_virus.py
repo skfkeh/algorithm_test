9e2823e142e756bdd714545c3121b80443b95b40
skfkeh <>
2023-04-29 21:55:32 +0900

    add b_2606_virus

---
+++ b_2606_virus.py
@@ -0,0 +1,35 @@
+### BAEKJOON
+# https://www.acmicpc.net/problem/2606
+def dfs(graph, root):
+    visited = []
+    stack = [root]
+    
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            if n in graph:
+                tmp = list(set(graph[n]) - set(visited))
+                tmp.sort(reverse=True)
+                stack += tmp
+                
+    return visited
+
+n = int(input())
+m = int(input())
+
+graph = {}
+
+for i in range(m):
+    n1, n2 = map(int, input().split())
+    if n1 not in graph:
+        graph[n1] = [n2]
+    elif n2 not in graph[n1]:
+        graph[n1].append(n2)
+        
+    if n2 not in graph:
+        graph[n2] = [n1]
+    elif n1 not in graph[n2]:
+        graph[n2].append(n1)
+        
+print(len(dfs(graph, 1))-1)
